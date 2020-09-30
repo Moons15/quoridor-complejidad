ee7c42cd876f576d57cb0a3d81e0802b498ad95a
Misael <misaticona@hotmail.com>
2020-09-30 13:12:39 -0500

    Player integration

--- src/player/human.py
+++ src/player/human.py
@@ -4,4 +4,33 @@ from src.action.IAction import *
 from src.action.quit import *
 
 
-
+class Human(IPlayer):
+    def play(self, board) -> IAction:
+        if not INTERFACE:
+            raise Exception("")
+        while True:
+            # TODO = CAPTURAMOS LA TECLA
+            key = board.window.getKey()
+            if key == ' ' or key == 'space':
+                validPawnMoves = board.storedValidPawnMoves[self.pawn.coord]
+                print(self.pawn.coord)
+                board.displayValidPawnMoves(self, validPawnMoves)
+                click = board.window.getMouse()
+                pawnMove = board.getPawnMoveFromMousePosition(self.pawn,
+                                                              click.x, click.y)
+                clickOnValidTarget = (pawnMove is not None)
+                board.hideValidPawnMoves(self, validPawnMoves)
+                if clickOnValidTarget:
+                    return pawnMove
+            if key == "f" or key == "F" and self.remainingFences() > 0:
+                # validFencePlacings = board.storedValidFencePlacings
+                # board.displayValidFencePlacings(self, validFencePlacings)
+                # TODO = Podemos colocar en la otra pantalla si activo para
+                #  las cercas
+                click = board.window.getMouse()
+                fencePlacing = board.getFencePlacingFromMousePosition(click.x,
+                                                                      click.y)
+                clickOnValidTarget = (fencePlacing is not None)
+                if clickOnValidTarget:
+                    return fencePlacing
+l
